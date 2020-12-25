16c20613833e1fd5983c07f701bc93db29fd01a9
Tanmay <iamtanmaykh07@gmail.com>
2020-12-25 17:36:39 +0530

    new updated

--- src/app.js
+++ src/app.js
@@ -1,20 +1,29 @@
 const express = require("express");
-const path = require("path")
+const path = require("path");
+const hbs =require('hbs')
 const app = express();
 const port =process.env.PORT || 8000;
 
 // public static path
 
 const static_path = path.join(__dirname,"../public")
+const template_path = path.join(__dirname,"../templates/views")
+const partials_path = path.join(__dirname,"../templates/partials")
+
+app.set('view engine' , 'hbs');
+app.set('views',template_path)
+hbs.registerPartials(partials_path);
 
 app.use(express.static(static_path));
 
 // Routing
 app.get("/" , (req,res) =>{
-    res.send("index")
+    res.render("index")
 })
 app.get("*", (req,res) =>{
-    res.send("404error")
+    res.render("404error",{
+        errorMsg: "OOps! Page Not Found"
+    })
 })
 
 // Port No
